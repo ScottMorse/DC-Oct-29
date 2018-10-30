4f6f76ad86376fc62efeb342742448f21837a69d
Scott Morse <scottmorsedev@gmail.com>
2018-10-29 22:31:47 -0500

    Added handling of SQL connection error

--- custom_modules/sqlCom.js
+++ custom_modules/sqlCom.js
@@ -5,7 +5,7 @@ let bcrypt = require('bcrypt')
 
 const dbName = 'expressToDo'
 
-const config = {
+let config = {
     host: "us-cdbr-iron-east-01.cleardb.net",
     user: "b186f7d42a6a43",
     password: "09481fe2",
@@ -22,10 +22,30 @@ function toDateTime(date){
     return newD.toISOString().slice(0, 19).replace('T', ' ')
 }
 
+function handleDisconnect() {
+    connection = mysql.createConnection(config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+}
+
 class Database {
     constructor( config ) {
-        this.connection = mysql.createConnection( config );
-        console.log('SQL connection successful')
+        this.connection = handleDisconnect();
     }
     query( sql, args ) {
         return new Promise( ( resolve, reject ) => {
